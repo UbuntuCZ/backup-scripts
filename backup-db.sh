585fd3c89fbac84023d77f036c060c9834453b71
Michal Stanke <michal@stanke.cz>
2019-03-24 10:23:02 +0100

    Cesta k ubuntu.cz

--- backup-db.sh
+++ backup-db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-db_name="$(grep "'database' =>" /var/www/ubuntu.cz/www/sites/www.ubuntu.cz/settings.php | cut -d "'" -f4)"
-db_user="$(grep "'username' =>" /var/www/ubuntu.cz/www/sites/www.ubuntu.cz/settings.php | cut -d "'" -f4)"
-db_pass="$(grep "'password' =>" /var/www/ubuntu.cz/www/sites/www.ubuntu.cz/settings.php | cut -d "'" -f4)"
+db_name="$(grep "'database' =>" /var/www/xubuntu.cz/www/sites/www.ubuntu.cz/settings.php | cut -d "'" -f4)"
+db_user="$(grep "'username' =>" /var/www/xubuntu.cz/www/sites/www.ubuntu.cz/settings.php | cut -d "'" -f4)"
+db_pass="$(grep "'password' =>" /var/www/xubuntu.cz/www/sites/www.ubuntu.cz/settings.php | cut -d "'" -f4)"
 
 mysqldump --user="$db_user" --password="$db_pass" --databases "$db_name" --lock-tables=false | gzip -c > '/tmp/ubuntucz.sql.gz'
